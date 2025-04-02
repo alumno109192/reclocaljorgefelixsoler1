5cafc9f6d7eb71fe3a6ba7d86afbd8ab6d257a7e
Jorge Félix <alumno.109192@ies-azarquiel.es>
2025-04-02 19:11:39 +0200

    tabla de multiplicar

--- develop1.java
+++ develop1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TablaMultiplicar {
+public class TablaMultiplicar { 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
